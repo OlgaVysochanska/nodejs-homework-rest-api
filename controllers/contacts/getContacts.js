3298ee2cb7bbb056f3346491e087772e6141a028
OlgaVysochanska <olavysochanska@gmail.com>
2023-04-14 14:54:07 +0300

    pagination works

--- controllers/contacts/getContacts.js
+++ controllers/contacts/getContacts.js
@@ -1,28 +1,27 @@
 const { Contact } = require("../../models");
-
-const getContacts = async (req, res, next) => {
-  try {
-    const { _id: owner } = req.user;
-    const { page = 1, limit = 20 } = req.query;
-    const skip = (page - 1) * limit;
-    // const { favourite } = req.query;
-    // if (favourite) {
-    //   const result = await Contact.find(
-    //     { owner },
-    //     { skip, limit },
-    //     { favourite }
-    //   ).populate("owner", "name email");
-    //   res.json(result);
-    //   return;
-    // }
-    const result = await Contact.find({ owner }, { skip, limit }).populate(
-      "owner",
-      "name email"
-    );
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
+const getContacts = async (req, res) => {
+  const { _id } = req.user;
+  const { page = 1, limit = 20, favorite = false } = req.query;
+  const skip = (page - 1) * limit;
+  const contacts = favorite
+    ? await Contact.find(
+        { owner: _id, favorite: true },
+        "-createdAt -updatedAt",
+        {
+          skip,
+          limit: Number(limit),
+        }
+      ).populate("owner", "email _id")
+    : await Contact.find({ owner: _id }, "-createdAt -updatedAt", {
+        skip,
+        limit: Number(limit),
+      }).populate("owner", "email _id");
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      result: contacts,
+    },
+  });
 };
-
 module.exports = getContacts;
